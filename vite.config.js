0bcaad0d973ae623ae1e0fe885138561d38f7a03
Aiman <aimannasser15@gmail.com>
2026-04-16 21:12:07 +0100

    Fix base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/phone-shop-pos/',  // must match your repo name exactly
+  base: '/',  // must match your repo name exactly
 })
